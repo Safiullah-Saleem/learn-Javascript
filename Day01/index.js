64b4c41c1f389da3730b250009f357e1e32f638b
Safiullah Saleem <safiullahsubadar@gmail.com>
2024-09-04 03:15:02 +0500

    first commit

---
+++ Day01/index.js
@@ -0,0 +1,36 @@
+// What is the value of age after this code runs?
+let age = 27;
+age + 2;
+console.log("The value of age is :", age);
+
+// what is the value of avg after this code runs?
+
+let hindi = 80;
+let eng = 90;
+let math = 100;
+let avg = (hindi + eng + math) / 3;
+console.log("The value of avg is:", avg);
+
+// Q: Declare your name as a string and print its length
+var fullName = "Safiullah Saleem";
+let length = fullName.length;
+console.log("The value of length is :", length);
+
+// Q:Declare your first name as a string and print its firsr letter
+
+let firstName = "Safiullah";
+console.log("The first letter of my first name is :", firstName[0]);
+
+//Q: Declare your first name as a string and print its last letter
+
+let firsrName = "Safiullah";
+console.log(
+  "The last letter of my first name is :",
+  firsrName[firsrName.length - 1]
+);
+
+
+//Q: what are length of an empty string with a single space?
+
+let emptyString = "  ";
+console.log("The length of an empty string with a single space is :", emptyString.length);
